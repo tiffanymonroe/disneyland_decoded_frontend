75d75024c70cc9d01e49eb78b94b6b7990f0b1db
Tiffany Monroe <hello@tiffanymonroe.com>
2017-09-17 22:19:50 -0700

    send credentials to the backend

--- public/app.js
+++ public/app.js
@@ -1,3 +1,23 @@
 console.log('Welcome to The Happiest Place on Earth');
 
 const app = angular.module("disney", []);
+
+app.controller('mainController', ['$http', function($http){
+
+  const controller = this;
+  this.url = 'http://localhost:3000';
+  this.user = {};
+
+  this.login = function(userPass){
+    console.log(userPass);
+    this.userPass = userPass;
+    $http({
+      method: 'post',
+      url: this.url + '/users/login',
+      data: {user: {username: userPass.username, password: userPass.password}},
+    }).then(function(res){
+      controller.user = res.data;
+    }.bind(this));
+  }
+
+}]); //end of mainController
