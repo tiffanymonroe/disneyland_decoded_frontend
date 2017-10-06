5c547276b366732b2f1ac11ade0ac9ce5b404ce5
Tiffany Monroe <hello@tiffanymonroe.com>
2017-10-05 23:14:02 -0700

    updated for backend

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ const app = angular.module("disney", []);
 app.controller('mainController', ['$http', '$scope', function($http, $scope){
 
   const controller = this;
-  this.url = 'https://disneyland-decoded-api.herokuapp.com'
+  this.url = 'http://localhost:3000'
 
 
   //user info
